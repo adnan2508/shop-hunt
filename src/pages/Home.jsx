d342c57a16c74fc2c85537d13f4e3fc55f662674
adnan2508 <adnandiu2800@gmail.com>
2024-08-18 01:01:07 +0600

    search and sort

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,52 @@ import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setProducts(data);
+        setItems(data);
       });
-  });
+  }, []);
+
+  const filterItems = (e) => {
+    setItems(products.filter(product => product.category === e));
+  };
+
   return (
     <div>
-      <h2 className="text-center mt-8">this is Home</h2>
+      <h2 className="text-center mt-8">this is Home: {items.length}</h2>
+
+      <div className="w-11/12 mx-auto flex gap-2 md:gap-8 mb-8">
+      <input type="text" placeholder="Search here..." className="input input-secondary max-w-xs" />
+      <select className="select select-secondary max-w-xs">
+  <option disabled selected>Sort By</option>
+  <option>Price(Low to High)</option>
+  <option>Price(High to Low)</option>
+  <option>Alphabetically(A-Z)</option>
+  <option>Alphabetically(Z-A)</option>
+</select>
+      </div>
+
+      {/* Filter buttons */}
+      <div className="w-11/12 mx-auto flex gap-3">
+        <div className="flex flex-col lg:flex-row gap-2">
+          <button className="btn btn-secondary" onClick={()=>setItems(products)}>All Products</button>
+          <button className="btn btn-secondary" onClick={()=>filterItems("men's clothing")}>men's clothing</button>
+          <button className="btn btn-secondary" onClick={()=>filterItems("women' clothing")}>women's clothing</button>
+          <button className="btn btn-secondary" onClick={()=>filterItems("jewelery")}>jewelery</button>
+          <button className="btn btn-secondary" onClick={()=>filterItems("electronics")}>electronics</button>
+        </div>
+      </div>
 
       {/* Product card */}
       <div className="w-11/12 mx-auto my-8 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        
+        {/* Mapping through products */}
         {products.map((product) => (
-          <div className="block border border-black-700 rounded-lg p-4 shadow-lg shadow-indigo-100">
+          <div key={product.id} className="block border border-black-700 rounded-lg p-4 shadow-lg shadow-indigo-100">
             <img
               alt=""
               src={product.image}
@@ -29,8 +57,7 @@ const Home = () => {
             <div className="mt-2">
               <dl>
                 <div>
-                  {/* <dt className="sr-only">Price</dt> */}
-
+                  
                   <dd className="text-sm text-gray-500">${product.price}</dd>
                 </div>
 
@@ -38,82 +65,25 @@ const Home = () => {
                   {/* <dt className="sr-only">Address</dt> */}
                   <dd className="font-medium">{product.title}</dd>
                 </div>
+
+                <p className="mt-2">
+                  {" "}
+                  {product.description.substring(0, 140) + "..."}
+                </p>
               </dl>
 
               <div className="mt-6 flex items-center gap-8 text-xs">
                 <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                  <svg
-                    className="size-4 text-indigo-700"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                    />
-                  </svg>
-
                   <div className="mt-1.5 sm:mt-0">
-                    <p className="text-gray-500">{product.category}</p>
-
-                    <p className="font-medium">2 spaces</p>
-                  </div>
-                </div>
-
-                <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                  <svg
-                    className="size-4 text-indigo-700"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-                    />
-                  </svg>
-
-                  <div className="mt-1.5 sm:mt-0">
-                    <p className="text-gray-500">Bathroom</p>
-
-                    <p className="font-medium">2 rooms</p>
-                  </div>
-                </div>
-
-                <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                  <svg
-                    className="size-4 text-indigo-700"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                    />
-                  </svg>
-
-                  <div className="mt-1.5 sm:mt-0">
-                    <p className="text-gray-500">Bedroom</p>
-
-                    <p className="font-medium">4 rooms</p>
+                    <div className="badge badge-secondary">
+                      {product.category}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         ))}
-
       </div>
     </div>
   );
